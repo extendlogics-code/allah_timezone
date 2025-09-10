c0328c19afd903538ac8da18e7b6087a9c3e24cf
Vasan <vasan@Vasans-Mac-mini.local>
2025-09-10 14:03:57 +0530

    chore: avoid Intelephense undefined type warning by using string class name for Dotenv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,11 +8,11 @@ $root = dirname(__DIR__);
 
 // Load environment if Dotenv is available and .env exists
 $envPath = $root . '/.env';
-if (class_exists(\Dotenv\Dotenv::class) && file_exists($envPath)) {
-    $dotenv = \Dotenv\Dotenv::createImmutable($root);
+if (class_exists('Dotenv\\Dotenv') && file_exists($envPath)) {
+    // Use string-based call to avoid static analysis error on undefined type
+    $dotenv = call_user_func(['Dotenv\\Dotenv', 'createImmutable'], $root);
     $dotenv->safeLoad();
 }
 
 // Default timezone from env, else UTC
 date_default_timezone_set($_ENV['APP_TIMEZONE'] ?? $_SERVER['APP_TIMEZONE'] ?? 'UTC');
-
